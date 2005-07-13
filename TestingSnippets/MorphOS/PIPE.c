52da91bf71e6035b3c3bffc4058e4e416cd75df4
aliem <aliem>
2005-07-13 23:13:41 +0000

    PIPE: device Test - initial import

---
+++ TestingSnippets/MorphOS/PIPE.c
@@ -0,0 +1,48 @@
+/* gcc -noixemul -o Execute-Test Execute-Test.c */
+/* =================================================================== */
+      /* Includes */
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <stdio.h>
+#include <string.h>
+
+/* =================================================================== */
+
+      /* Vars */
+BPTR OutFile;
+BOOL Command;
+
+
+      /* Consts */
+STRPTR CommandLine = "lha";
+
+/* =================================================================== */
+
+int
+main (int argc, char argv)
+{
+
+    OutFile = Open("PIPE:Output", MODE_READWRITE);
+
+    Command = Execute(CommandLine,NULL,OutFile);
+
+    if (Command)
+        Close(OutFile);
+    else
+        return RETURN_ERROR;
+
+    // stampa a video PIPE:OUTPUT
+
+    // Cancella PIPE:OUTPUT
+
+    return RETURN_OK;
+
+}
+
+
+/* =================================================================== */
+/*
+   BPTR    = File Pointer
+   STRPTR  = String Pointer
+
+*/
