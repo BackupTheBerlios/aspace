55a9c6a4f01ae777b7d8f1d61747bf81036d2dd6
aliem <aliem>
2005-07-14 16:56:53 +0000

    main.c skeleton

---
+++ mangabook/main.c
@@ -0,0 +1,24 @@
+/* -------------------------------------------------------------------------------------
+    Under GNU LGPL
+*/
+
+/*
+** MangaBook will be a ComicBook reader. Will be fast, small ... and only for MorphOS ;)
+*/
+
+/* includes */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* prototypes */
+
+extern int main(int argc, char **argv);
+
+/* program */
+
+int
+main(int argc, char **argv)
+{
+    exit(0);
+}
